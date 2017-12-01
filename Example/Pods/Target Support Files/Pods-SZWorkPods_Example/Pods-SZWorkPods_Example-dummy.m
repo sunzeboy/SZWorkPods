8da64099ee3df1909ead8d7ca927ca62233e6db0
Joe <13162153278@163.com>
2017-12-01 16:24:51 +0800

    set up project

---
+++ Example/Pods/Target Support Files/Pods-SZWorkPods_Example/Pods-SZWorkPods_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SZWorkPods_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SZWorkPods_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-SZWorkPods_Tests/Pods-SZWorkPods_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SZWorkPods_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SZWorkPods_Tests
+@end
